fabe907f6fe39c8d3155eab0e870e11cb51e6f3f
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-09 10:23:41 +0000

    added Player2 points visibility for player 1

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,8 +1,8 @@
 require 'capybara/rspec'
 
-describe Battle do
+describe 'Battle', :type => :feature do
 
-  describe "Homepage", :type => :feature do
+  describe "Homepage" do
     it 'should say "Hello players!!"' do
       visit '/'
       expect(page).to have_content "Hello players!!"
@@ -11,9 +11,17 @@ describe Battle do
       visit '/'
       fill_in 'player1', with: "JJ"
       fill_in 'player2', with: "Marie"
-      click_button 'Save'
-      expect(page).to have_content "JJ"
-      expect(page).to have_content "Marie"
+      click_button 'Start'
+      expect(page).to have_content "JJ vs Marie"
+    end
+  end
+  context ' When starting a game' do
+    it 'should display User 2 hitpoints to User 1' do
+    visit '/'
+    fill_in 'player1', with: 'Cris'
+    fill_in 'player2', with: 'JJ'
+    click_button 'Start'
+    expect(page).to have_content 'Player2 - HP = 10'
     end
   end
 end
